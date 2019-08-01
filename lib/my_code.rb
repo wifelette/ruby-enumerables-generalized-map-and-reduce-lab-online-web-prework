6a8cc39613d103c24e4c429e430f557492391de6
Leah Silber <leahsilber@gmail.com>
2019-08-01 19:02:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+def map(array)
+  array.map! do |item|
+    yield item
+  end
+end
+
+array = [1,2,3, -9]
+
+map(array) do |n|
+  n * -1
+end
+
+def reduce(array, starting_point = 0)
+  array.reduce do |item|
+    yield item
+  end
+end
+
+reduce(array, starting_point = 0) do |n|
+  total = starting_point + n
+end
